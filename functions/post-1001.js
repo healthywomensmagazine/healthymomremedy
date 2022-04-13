7f1e3a5b2f27e3ad5ece3ed29d67ca7bd92be9de
healthywomensmagazine <58111349+healthywomensmagazine@users.noreply.github.com>
2022-04-13 14:40:43 +0200

    Update post-1001.js

--- functions/post-1001.js
+++ functions/post-1001.js
@@ -1,19 +1,8 @@
-exports.handler = async event => {
-  if (event.headers.referer.includes('pinterest')) {
-    return {
-      statusCode: 301,
-      headers: {
-        'cache-control': 'public, max-age=0, must-revalidate',
-        location: decodeURIComponent(event.queryStringParameters.url)
-      }
-    }
-  } else {
-    return {
-      statusCode: 301,
-      headers: {
-        'cache-control': 'public, max-age=0, must-revalidate',
-        location: process.env.URL + '/' + decodeURIComponent(event.queryStringParameters.url).split('/')[3] + '/'
-      }
+exports.handler = async () => {
+  return {
+    statusCode: 301,
+    headers: {
+      location: 'https://www.google.com/'
     }
   }
 }
