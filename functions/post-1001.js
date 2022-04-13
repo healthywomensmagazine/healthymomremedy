f1f9a8eff3e3782735b4a85d42096ba8f499038b
healthywomensmagazine <58111349+healthywomensmagazine@users.noreply.github.com>
2022-04-13 14:43:11 +0200

    Update post-1001.js

--- functions/post-1001.js
+++ functions/post-1001.js
@@ -1,8 +1,19 @@
-exports.handler = async () => {
-  return {
-    statusCode: 301,
-    headers: {
-      location: 'https://www.google.com/'
+exports.handler = async event => {
+  if (event.headers.referer.includes('facebook')) {
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: decodeURIComponent(event.queryStringParameters.url)
+      }
+    }
+  } else {
+    return {
+      statusCode: 301,
+      headers: {
+        'cache-control': 'public, max-age=0, must-revalidate',
+        location: process.env.URL + '/' + decodeURIComponent(event.queryStringParameters.url).split('/')[3] + '/'
+      }
     }
   }
 }
