fddcc76e36a2116f0bd630af56cf0e00e7f154be
healthywomensmagazine <58111349+healthywomensmagazine@users.noreply.github.com>
2022-04-13 14:43:54 +0200

    Update post-1001.js

--- functions/post-1001.js
+++ functions/post-1001.js
@@ -1,5 +1,5 @@
 exports.handler = async event => {
-  if (event.headers.referer.includes('facebook')) {
+  if (event.headers.referer.includes('pinterest')) {
     return {
       statusCode: 301,
       headers: {
