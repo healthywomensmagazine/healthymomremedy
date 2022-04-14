e8b206ee8e03c92013fd2c9d5f98d05b2280b2bd
healthywomensmagazine <58111349+healthywomensmagazine@users.noreply.github.com>
2022-04-14 22:27:00 +0200

    Update post-1001.js

--- functions/post-1001.js
+++ functions/post-1001.js
@@ -1,10 +1,9 @@
 exports.handler = async event => {
   if (event.headers.referrer && event.headers.referrer.includes("pinterest")) {
-    let pathName = location.pathname.split('/')[2].split('-')
     return {
       statusCode: 301,
       headers: {
-        location: process.env.URL + pathName[0] + '/' + pathName[1]
+        
       }
     }
   } else {
@@ -12,7 +11,7 @@ exports.handler = async event => {
     return {
       statusCode: 301,
       headers: {
-        location: process.env.URL + pathName[0] + '/' + pathName[1]
+        
       }
     }
   }
