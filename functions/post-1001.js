80da5864134e452c57c1ef36b660cbc0f4a66fd0
healthywomensmagazine <58111349+healthywomensmagazine@users.noreply.github.com>
2022-04-14 22:05:59 +0200

    Update post-1001.js

--- functions/post-1001.js
+++ functions/post-1001.js
@@ -1,4 +1,5 @@
 exports.handler = async event => {
+  let pathName = location.pathname.split('/')[2].split('-')
   location: process.env.URL + pathName[0] + '/' + pathName[1]
   if (event.headers.referrer && event.headers.referrer.includes('pinterest')) {
     return {
