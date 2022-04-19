e9a156737b46a248043f4a88710ba7f70ec6484b
healthywomensmagazine <58111349+healthywomensmagazine@users.noreply.github.com>
2022-04-19 21:00:23 +0200

    Update post.js

--- functions/post.js
+++ functions/post.js
@@ -1,17 +1,18 @@
 const fs = require('fs')
 const MarkdownIt = require('markdown-it')
-
-exports.handler = async event => {
-  if (event.headers.referer && event.headers.referer.includes("pinterest")) {
+   
+exports.handler = async (event) => {
+	if (event.headers.referer && event.headers.referer.includes("pinterest")) {
     return {
       statusCode: 301,
       headers: {
         location: 'https://www.google.com/'
       }
     }
-  } else {
+  }
+	else {
     const { postId } = event.queryStringParameters;
-    const fileContents = fs.readFileSync(`./post/post-${postId}.md`, 'utf8')
+    const fileContents = fs.readFileSync(`./posts/post-${postId}.md`, 'utf8')
     const md = new MarkdownIt();
     const result = md.render(fileContents);
     
@@ -23,5 +24,5 @@ exports.handler = async event => {
         },
         body: result,
     }
-  }
+	}
 }
